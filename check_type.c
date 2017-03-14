7cc048ec44cb05e8f3278d64b04559620c905c90
Ivan Tsuman <itsuman@e1r5p6.unit.ua>
2017-03-14 17:50:26 +0200

    Have changed file check_type.c for work with type 'p'.

--- check_type.c
+++ check_type.c
@@ -6,7 +6,7 @@
 /*   By: itsuman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/15 18:15:26 by itsuman           #+#    #+#             */
-/*   Updated: 2017/03/14 17:12:03 by itsuman          ###   ########.fr       */
+/*   Updated: 2017/03/14 17:48:47 by itsuman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,20 @@ void	check_type(const char *format, t_strf *f)
 	type = "sSpdDioOuUxXcC";
 	while (is_spesify(format[i]))
 	{
-		j = 0;
-		while (type[j] != '\0')
-		{
+		j = -1;
+		while (type[++j] != '\0')
 			if (type[j] == format[i])
 			{
 				f->type = format[i];
+				if (f->type == 'p')
+				{
+					f->type = 'x';
+					f->mod_size = 'l';
+					f->pr = 3;
+					f->flag[0] = '#';
+				}
 				return ;
 			}
-			j++;
-		}
 		i++;
 	}
-	if (f->type == 'p')
-		f->type = 'x';
 }
